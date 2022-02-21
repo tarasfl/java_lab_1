b1d4d522895d6c66e95c63bd0b4bfa6d46cd5204
tarasfl <tfliorko@gmail.com>
2022-02-21 23:09:33 +0200

    Created files

---
+++ main.java
@@ -0,0 +1,10 @@
+public class main {
+    public static void main(String[] args) {
+        Radiator r1 = new Radiator();
+        Radiator r2 = new Radiator("nvesd", 13, "tak", "tabir", 9.6, 6);
+        Radiator r3 = new Radiator("tak treba", 67, "scho", "89", 65446, 1);
+        System.out.println(r1);
+        System.out.println(r2);
+        System.out.println(r3);
+    }
+}
